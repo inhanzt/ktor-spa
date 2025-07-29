727557f3103fdcf4c39a0feeef43f027625d8be4
Kyle Sexton <ksexton@cog.systems>
2025-07-29 13:05:25 -0400

    rip out bintray -- it doesn't actually exist to commit to anymore I think.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,5 @@
 @file:Suppress("UNUSED_VARIABLE")
 
-import com.jfrog.bintray.gradle.BintrayExtension
-import com.jfrog.bintray.gradle.tasks.BintrayUploadTask
-
 buildscript {
 
     repositories {
@@ -71,44 +68,6 @@ kotlin {
             }
         }
     }
-}
-
-bintray {
-    user = searchPropertyOrNull("bintrayUsername", "BINTRAY_USERNAME")
-    key = searchPropertyOrNull("bintrayApiKey", "BINTRAY_API_KEY")
-    pkg {
-        version {
-            name = project.version as String
-        }
-        repo = "com.github.lamba92"
-        name = "ktor-spa"
-        setLicenses("Apache-2.0")
-        vcsUrl = "https://github.com/lamba92/ktor-spa"
-        issueTrackerUrl = "https://github.com/lamba92/ktor-spa/issues"
-    }
-    publish = true
-    setPublications(*publishing.publications.names.toTypedArray())
-}
-
-/* tasks.withType<BintrayUploadTask> {
-    doFirst {
-        publishing.publications.withType<MavenPublication> {
-            buildDir.resolve("publications/$name/module.json").let {
-                if (it.exists())
-                    artifact(object : org.gradle.api.publish.maven.internal.artifact.FileBasedMavenArtifact(it) {
-                        override fun getDefaultExtension() = "module"
-                    })
-            }
-        }
-    }
-} */
-
-fun BintrayExtension.pkg(action: BintrayExtension.PackageConfig.() -> Unit) {
-    pkg(closureOf(action))
-}
-
-fun BintrayExtension.PackageConfig.version(action: BintrayExtension.VersionConfig.() -> Unit) {
-    version(closureOf(action))
 }
 
 fun searchPropertyOrNull(name: String, vararg aliases: String): String? {
